e5b59099d31506e49f989fe01680732eb2bbb806
Peevee Jace G. Pascual <pv.pascual@gmail.com>
2020-05-16 18:27:33 -0700

    Backend project added

---
+++ backend/Angular6ByExample/Models/Exercise.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular6ByExample.Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Duration { get; set; }
+        public WorkoutPlan WorkoutPlan { get; set; }
+    }
+}
---
+++ backend/Angular6ByExample/Models/WorkoutPlanContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular6ByExample.Models
+{
+    public class WorkoutPlanContext: DbContext
+    {
+        public WorkoutPlanContext(DbContextOptions<WorkoutPlanContext> options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+        //entities
+        public DbSet<WorkoutPlan> WorkoutPlans { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+    }
+}
